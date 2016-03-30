50c4a41cf6dc8cbca5079b9e6cf7a29ba9f2b5b2
Shalin LK <shalinlk@hifx.co.in>
2016-03-30 11:37:18 +0530

    attacker separated; sitll not working moduel

--- client/publisher.go
+++ client/publisher.go
@@ -23,19 +23,15 @@ func NewDurationBasedAttacker(duration time.Duration, conn *Conn) *AttackPlan {
 	attacker := newPlanePlanner()
 	conn.RegisterPublisher(attacker.deliveryNozzle)
 	go func() {
-		attacker.doneSignal.Add(1)
-		defer attacker.doneSignal.Done()
-		<-attacker.ignite
-		stopper := time.NewTimer(duration)
-		for {
-			select {
-			case attacker.deliveryNozzle <- attacker.publisher.deliveryNozzle:
-			case <-stopper.C:
-				break
-			}
-		}
-		attacker.publisher.done()
-		close(attacker.deliveryNozzle)
+		attacker.attack(func() {
+			stopper := time.NewTimer(duration)
+			for {
+				select {
+				case attacker.deliveryNozzle <- attacker.publisher.deliveryNozzle:
+				case <-stopper.C:
+					break
+				}
+			} })
 	}()
 	return attacker
 }
@@ -44,14 +40,11 @@ func NewMsgCountBasedAttacker(count int, conn *Conn) *AttackPlan {
 	attacker := newPlanePlanner()
 	conn.RegisterPublisher(attacker.deliveryNozzle)
 	go func() {
-		attacker.doneSignal.Add(1)
-		defer attacker.doneSignal.Done()
-		<-attacker.ignite
-		for i := 0; i < count; i++ {
-			attacker.deliveryNozzle <- attacker.publisher.deliveryNozzle
-		}
-		attacker.publisher.done()
-		close(attacker.deliveryNozzle)
+		attacker.attack(func() {
+			for i := 0; i < count; i++ {
+				attacker.deliveryNozzle <- attacker.publisher.deliveryNozzle
+			}
+		})
 	}()
 	return attacker
 }
@@ -70,10 +63,19 @@ func (a *AttackPlan)LaunchAttack(startTime time.Time, publisher *Publisher) (err
 	return nil
 }
 
+func (a *AttackPlan)attack(attackController func()) {
+	a.doneSignal.Add(1)
+	defer a.publisher.done()
+	defer a.doneSignal.Done()
+	<-a.ignite
+	attackController()
+	close(a.deliveryNozzle)
+}
+
 type Publisher struct {
 	deliveryNozzle chan models.OutgoingPacket
 	topics         map[string]int
-	doneSignal	   chan struct{}
+	doneSignal     chan struct{}
 }
 
 func (p *Publisher)NewPublisher(topics models.Topics) (*Publisher, error) {
@@ -98,9 +100,9 @@ func (p *Publisher)run() {
 				msg.QoS = qos
 				msg.Topic = topic
 				select {
-				case <- p.doneSignal:
+				case <-p.doneSignal:
 					break
-				default :
+				default:
 					go func(msg models.OutgoingPacket) {
 						p.deliveryNozzle <- msg
 					}(msg)
@@ -110,8 +112,8 @@ func (p *Publisher)run() {
 	}
 }
 
-func (p *Publisher)done()  {
-	for i := 0 ;i < len(p.topics); i++ {
+func (p *Publisher)done() {
+	for i := 0; i < len(p.topics); i++ {
 		p.doneSignal <- struct{}
 	}
 }
